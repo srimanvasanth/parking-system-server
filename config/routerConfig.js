af681954f3a64f082de537d72cfd0e0b3f7930ab
Sriman Vasanth <147894546+srimanvasanth@users.noreply.github.com>
2026-04-28 13:12:38 +0530

    Update routerConfig.js

--- config/routerConfig.js
+++ config/routerConfig.js
@@ -5,7 +5,9 @@ const authMiddleware = require("../security/authMiddleware");
 const parkingController = require("../controller/parkingController")
 const { userRouter, loginHandler, saveUser } = require("../controller/userController");
 
-
+router.post("/", (req, res) => {
+  res.send("server up")
+})
 router.post("/userLogin", loginHandler);
 router.post("/createUser", saveUser);  
 
