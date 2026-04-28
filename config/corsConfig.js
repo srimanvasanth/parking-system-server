53acdb7f50e006dc7ab40e1762e02075b7c2fc2d
Sriman Vasanth <147894546+srimanvasanth@users.noreply.github.com>
2026-04-28 14:38:46 +0530

    Update corsConfig.js

--- config/corsConfig.js
+++ config/corsConfig.js
@@ -1,5 +1,5 @@
 const corsConfig = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.header("Access-Control-Allow-Origin", "https://parking-system-server.onrender.com");
     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Authorization, Accept, Content-Type");
     res.header("Access-Control-Allow-Credentials", "true");
